8059c4a692f7af94f77581de54e095aa5e6605dd
孔尚杰 <kongsj@zz91.net>
2012-04-23 16:40:15 +0800

    完成:sms发送接口controller建立

---
+++ zz91-sms/src/main/java/com/zz91/sms/controller/SMSController.java
@@ -0,0 +1,11 @@
+package com.zz91.sms.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class SMSController extends BaseController{
+	
+	@RequestMapping
+	public void send(){
+//		sms_log
+	}
+}
