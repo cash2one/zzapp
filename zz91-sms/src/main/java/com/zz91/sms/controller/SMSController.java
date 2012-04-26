315a5db5d35e9009ff28ec499c60143bbcb5a5ec
李大方 <thinkldf@gmail.com>
2012-04-26 08:40:01 +0800

    完成：发送类的创建

--- zz91-sms/src/main/java/com/zz91/sms/controller/SMSController.java
+++ zz91-sms/src/main/java/com/zz91/sms/controller/SMSController.java
@@ -1,8 +1,17 @@
 package com.zz91.sms.controller;
 
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.zz91.sms.service.smslog.SmsLogService;
+
+@Controller
 public class SMSController extends BaseController{
+	
+	@Resource
+	private SmsLogService smsLogService;
 	
 	@RequestMapping
 	public void send(){
