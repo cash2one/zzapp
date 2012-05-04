fcb0903daaa410d14e0a421a1baac8e08904b51a
孔尚杰 <kongsj@zz91.net>
2012-05-04 11:47:35 +0800

    fixed:修改初始化线程类(initthread),删除发送线程启动的代码

--- zz91-sms/src/main/java/com/zz91/sms/config/InitThread.java
+++ zz91-sms/src/main/java/com/zz91/sms/config/InitThread.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Component;
 
 import com.zz91.sms.thread.ControlThread;
 import com.zz91.sms.thread.SmsScanThread;
-import com.zz91.sms.thread.SmsSendThread;
 
 @Component("initThread")
 public class InitThread {
@@ -17,13 +16,10 @@ public class InitThread {
 	private SmsScanThread smsScanThread;
 	@Resource
 	private ControlThread controlThread;
-//	@Resource
-//	private SmsSendThread smsSendThread;
 	
 	public void startup(){
 		controlThread.start();
 		smsScanThread.start();
-//		smsSendThread.start();
 	}
 	
 	public void destroy(){
