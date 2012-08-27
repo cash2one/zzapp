8b9457bcd04abea354c46bb4d77932c076c2dfbb
黄怀清 <huanghqasto@gmail.com>
2012-08-27 11:46:06 +0800

    日志系统mongodb版,增加日志记录查询接口

---
+++ zz91-log/src/main/java/com/zz91/log/servlet/LogRecordServlet.java
@@ -0,0 +1,98 @@
+package com.zz91.log.servlet;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONObject;
+import net.sf.json.JSONSerializer;
+
+import org.apache.commons.httpclient.NameValuePair;
+import org.apache.log4j.Logger;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+import com.mongodb.util.JSON;
+import com.mongodb.util.JSONParseException;
+import com.zz91.log.thread.LogThread;
+import com.zz91.util.lang.StringUtils;
+
+
+
+
+/**
+ * 项目名称：日志统计
+ * 模块描述：接收日志数据servlet
+ * 变更履历：修改日期　　　　　修改者　　　　　　　版本号　　　　　修改内容
+ *　　　　　 2012-08-09　　　黄怀清　　　　　　　1.0.0　　　　　原插入文本改为mongDB
+ */
+@SuppressWarnings("serial")
+public class LogRecordServlet extends HttpServlet {
+	
+	public static int NUM_LOG=0;
+	
+	final static Logger LOG= Logger.getLogger("com.zz91.log4z");
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		LogThread.init();
+		Thread thr=new Thread(new LogThread());
+		thr.start();
+		super.init();
+	}
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		LogThread.destroy();
+		super.destroy();
+	}
+	
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		
+		
+//		try {
+			Map<String,Object> map =new HashMap<String, Object>();
+			if (request.getParameter("appCode")!=null) {
+				map.put("appCode", request.getParameter("appCode"));
+			}
+			if (request.getParameter("operator")!=null) {
+				map.put("operator", request.getParameter("operator").toString());
+			}
+			if (request.getParameter("operation")!=null) {
+				map.put("operation", request.getParameter("operation").toString());
+			}
+			if (request.getParameter("ip")!=null) {
+				map.put("ip", request.getParameter("ip").toString());
+			}
+			if (request.getParameter("time")!=null) {
+				map.put("time", request.getParameter("time").toString());
+			}
+			if (request.getParameter("data")!=null) {
+				if(request.getParameter("data").startsWith("{")){
+					map.put("data", JSONSerializer.toJSON(request.getParameter("data")));
+				}else{
+					map.put("data", request.getParameter("data").toString());
+				}
+				
+			}
+			
+			if(!LogThread.logsQueue.offer(map)){
+				LOG.debug("insert error:full!");
+			}
+			
+//		} catch (JSONParseException e) {
+//			System.out.println("json转换出错!");
+//		}
+		
+		
+	}
+
+}
--- zz91-log/src/main/java/com/zz91/log/thread/LogThread.java
+++ zz91-log/src/main/java/com/zz91/log/thread/LogThread.java
@@ -5,32 +5,104 @@
  */
 package com.zz91.log.thread;
 
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 import org.apache.log4j.Logger;
 
-import com.zz91.log.service.LogService;
-import com.zz91.util.log.LogInfo;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+import com.mongodb.Mongo;
+import com.mongodb.MongoOptions;
+import com.zz91.util.file.FileUtils;
 
 /**
- * @author Leon
- * 
+ * 项目名称：日志统计
+ * 模块描述：数据插入Thread
+ * 变更履历：修改日期　　　　　修改者　　　　　　　版本号　　　　　修改内容
+ *　　　　　 2012-08-09　　　黄怀清　　　　　　　1.0.0　　　　　原插入文本改为mongDB
  */
 public class LogThread implements Runnable {
 
+	/*
+	 * 日志队列,测试版,此同步方式可能不是线程安全的,后续优化
+	 */
+	public static BlockingQueue<Map<String, Object>> logsQueue = new LinkedBlockingQueue<Map<String,Object>>(10000);
+	private static Mongo mongo = null;
+	private static DB db = null;
+	private static DBCollection dbc = null;
 	private static Logger LOG=Logger.getLogger(LogThread.class);
-	private LogInfo logInfo;
 
 	public LogThread() {
 
 	}
 
-	public LogThread(LogInfo log) {
-		this.logInfo = log;
+//	public LogThread(LogInfo log) {
+//		this.logInfo = log;
+//	}
+	public static void init(){
+		Map<String, String> map =null;
+		try {
+			 map = FileUtils.readPropertyFile("file:/usr/tools/config/db/db-zzlog-mongo.properties", "utf-8");
+	        
+		} catch (Exception e) {
+			LOG.error("read propertyFile Exception:"+e.getMessage());
+		}
+		try {
+			if(map!=null){
+				//连接池配置
+				MongoOptions options = new MongoOptions(); 
+		        options.autoConnectRetry = true; 
+		        options.connectionsPerHost = 20; 
+		        options.connectTimeout = 0; 
+		        options.maxAutoConnectRetryTime = 12000; 
+		        options.maxWaitTime = 12000; 
+		        options.socketKeepAlive = true; 
+		        options.socketTimeout = 0;
+				
+				mongo = new Mongo(map.get("mongo.host"),options);
+				db = mongo.getDB(map.get("mongo.db"));
+				dbc = db.getCollection(map.get("mongo.collection"));
+				
+			}
+		} catch (Exception e) {
+			LOG.error("MongoDB connection Exception:"+e.getMessage());
+		}
+	}
+	public static void destroy(){
+		logsQueue.clear();
+		mongo.close();
 	}
 	
+	/**
+	 * 此处插入数据,测试版,现为单机跑,后续依数据插入时间和效率看是否需要线程池托管跑多个线程.
+	 */
 	@Override
 	public void run() {
-		LogService.getInstance().insertLog(logInfo);
-		LOG.debug("LogService.getInstance().insertLog(logInfo) run success!");
+		
+		while(true){
+			try {
+				DBObject dbo = new BasicDBObject();
+				//取出日志队列数据存入mongo
+				Map<String, Object> data = logsQueue.take();
+								
+				dbo.putAll(data);
+				
+				dbc.save(dbo);
+				
+				
+			} catch (Exception e) {
+//				mongo.close();
+				LOG.debug(">>"+e.getMessage());
+			}
+		}
+		
+		
+//		LogService.getInstance().insertLog(logInfo);
+//		LOG.debug("LogService.getInstance().insertLog(logInfo) run success!");
 		
 	}
 
